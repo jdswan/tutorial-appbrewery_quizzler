c4e8e7dfa7d8a10b0a5d8bd529e4da7626868884
JDSwan <jeffdswan@gmail.com>
2019-07-18 18:15:13 -0500

    Mostly completed. Scorekeeper still overflows.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,62 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
+  int questionNumber = 0;
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [
+    false,
+    true,
+    true
+  ];
+
+  void incrementQuestion() {
+    setState(() {
+      if (questionNumber < questions.length - 1) {
+        questionNumber++;
+      } else {
+        questionNumber = 0;
+      }
+    });
+  }
+
+  bool checkAnswer(bool answer) {
+    if (answers[questionNumber] == answer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void addScore(bool correct) {
+    if (correct) {
+      setState(() {
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      });
+    } else {
+      setState(() {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +93,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +117,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                addScore((checkAnswer(true)));
+                incrementQuestion();
               },
             ),
           ),
@@ -79,12 +136,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                addScore((checkAnswer(false)));
+                incrementQuestion();
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
